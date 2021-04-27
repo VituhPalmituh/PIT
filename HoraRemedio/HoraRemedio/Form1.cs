e4a1f97a8559fcd67af1d2794d3b7a53e66b7d76
VituhPalmituh <vitorferreiradsilva@gmail.com>
2021-04-27 15:26:22 -0300

    Arrumando resolução

--- HoraRemedio/HoraRemedio/Form1.cs
+++ HoraRemedio/HoraRemedio/Form1.cs
@@ -16,5 +16,10 @@ namespace HoraRemedio
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
